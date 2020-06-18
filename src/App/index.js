7806b1eefb89afb15df88fbf61748ed6955ab8bc
Jeremy Retailleau <jeremy.retailleau@gmail.com>
2020-06-17 20:25:56 -0700

    Fix name

--- src/App/index.js
+++ src/App/index.js
@@ -30,7 +30,7 @@ export default function App() {
             >
                 <Toolbar>
                     <Typography variant="h6" noWrap>
-                        Color Interpo
+                        Color Interpolation
                     </Typography>
                 </Toolbar>
             </AppBar>
